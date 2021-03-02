d91ab96d1a665378370753cd118d93d270e10da8
WADEOS <wade4615@gmail.com>
2021-03-02 08:56:20 -0600

    added SpringCucumber in a basic form

---
+++ SpringCucumber/src/test/java/com/wade/spring/examples/SpringCucumber/SpringCucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wade.spring.examples.SpringCucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCucumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

80f606bb807c61bc66780763b461d6c457560ad9
WADEOS <wade4615@gmail.com>
2021-03-02 07:26:36 -0600

    added all projects

---
+++ SpringJPAMVC/src/test/java/com/wade/spring/examples/jpa/mvc/SpringJpamvcApplicationTests.java
@@ -0,0 +1,10 @@
+package com.wade.spring.examples.jpa.mvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpamvcApplicationTests {
+//	@Test
+//	void contextLoads() {
+//	}
+}

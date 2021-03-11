625ea1abf634190f7a2800d207b9f6ecbfd988c7
WADEOS <wade4615@gmail.com>
2021-03-11 15:53:03 -0600

    updtaed to include JUnit5 tests

--- SpringJPAMVC/src/main/java/com/wade/spring/examples/jpa/mvc/model/Employee.java
+++ SpringJPAMVC/src/main/java/com/wade/spring/examples/jpa/mvc/model/Employee.java
@@ -1,5 +1,7 @@
 package com.wade.spring.examples.jpa.mvc.model;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -39,36 +41,16 @@ public class Employee {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (!(obj instanceof Employee)) {
 			return false;
-		if (getClass() != obj.getClass())
-			return false;
+		}
 		Employee other = (Employee) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (id != other.id)
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		if (title == null) {
-			if (other.title != null)
-				return false;
-		} else if (!title.equals(other.title))
-			return false;
-		return true;
+		return Objects.equals(email, other.email) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(id, other.id) && Objects.equals(lastName, other.lastName)
+				&& Objects.equals(title, other.title);
 	}
 
 	public String getEmail() {
@@ -93,14 +75,7 @@ public class Employee {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-		result = prime * result + ((title == null) ? 0 : title.hashCode());
-		return result;
+		return Objects.hash(email, firstName, id, lastName, title);
 	}
 
 	public void setEmail(String email) {

1b45ae64ae8126adeb6ffa37712ffb8d288fb935
WADEOS <wade4615@gmail.com>
2021-03-17 13:22:36 -0500

    update to secuity jpas unit test to improve code coverage

--- SpringSecurityJPA/src/test/java/com/wade/spring/examples/SpringSecurityJPA/service/MyUserDetailsServiceTest.java
+++ SpringSecurityJPA/src/test/java/com/wade/spring/examples/SpringSecurityJPA/service/MyUserDetailsServiceTest.java
@@ -5,6 +5,8 @@ import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
 
+import javax.persistence.Id;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,7 +17,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.wade.spring.examples.SpringSecurityJPA.model.Role;
+import com.wade.spring.examples.SpringSecurityJPA.model.Role.RoleBuilder;
 import com.wade.spring.examples.SpringSecurityJPA.model.User;
+import nl.jqno.equalsverifier.EqualsVerifier;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -27,8 +31,48 @@ class MyUserDetailsServiceTest {
 	private MyUserDetailsService service;
 
 	@Test
-	void testLoadUserByUsername() {
-		Role role = Role.builder().id(1).role("role").build();
+	void testLoadUserByUsername1() {
+		RoleBuilder builder = Role.builder();
+		Role role = builder.id(1).role("role").build();
+		HashSet<Role> roles = new HashSet<>();
+		roles.add(role);
+		User user = User.builder().id(1).userName("GPonce").email("test@test.com").name("Gustavo").lastName("Ponce")
+				.password("pasword").active(true).roles(roles).build();
+		String userName = "GPonce";
+		when(mockUserService.findUserByUserName(userName)).thenReturn(user);
+		UserDetails result = service.loadUserByUsername(userName);
+		assertNotNull(result);
+		assertNotNull(result.getUsername());
+		assertNotNull(result.getPassword());
+		assertNotNull(result.getAuthorities());
+		assertNotNull(role.toString());
+		assertNotNull(builder.toString());
+        EqualsVerifier.forClass(role.getClass()).withIgnoredAnnotations(Id.class).verify();
+	}
+	
+	@Test
+	void testLoadUserByUsername2() {
+		Role role = new Role(1,"role");
+		HashSet<Role> roles = new HashSet<>();
+		roles.add(role);
+		User user = User.builder().id(1).userName("GPonce").email("test@test.com").name("Gustavo").lastName("Ponce")
+				.password("pasword").active(true).roles(roles).build();
+		String userName = "GPonce";
+		when(mockUserService.findUserByUserName(userName)).thenReturn(user);
+		UserDetails result = service.loadUserByUsername(userName);
+		assertNotNull(result);
+		assertNotNull(result.getUsername());
+		assertNotNull(result.getPassword());
+		assertNotNull(result.getAuthorities());
+		assertNotNull(result.hashCode());
+        EqualsVerifier.forClass(role.getClass()).withIgnoredAnnotations(Id.class).verify();
+	}
+	
+	@Test
+	void testLoadUserByUsername3() {
+		Role role = new Role();
+		role.setId(1);
+		role.setRole("role");
 		HashSet<Role> roles = new HashSet<>();
 		roles.add(role);
 		User user = User.builder().id(1).userName("GPonce").email("test@test.com").name("Gustavo").lastName("Ponce")
--- SpringSecurityJPA/src/test/java/com/wade/spring/examples/SpringSecurityJPA/service/UserServiceTest.java
+++ SpringSecurityJPA/src/test/java/com/wade/spring/examples/SpringSecurityJPA/service/UserServiceTest.java
@@ -6,6 +6,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import javax.persistence.Id;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,8 +18,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.wade.spring.examples.SpringSecurityJPA.model.User;
+import com.wade.spring.examples.SpringSecurityJPA.model.User.UserBuilder;
 import com.wade.spring.examples.SpringSecurityJPA.repository.RoleRepository;
 import com.wade.spring.examples.SpringSecurityJPA.repository.UserRepository;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -34,13 +39,17 @@ class UserServiceTest {
 
 	@Test
 	public void testFindByUserName() {
-		User user = User.builder().id(1).userName("GPonce").name("Gustavo").lastName("Ponce").email("test@test.com")
+		UserBuilder builder = User.builder();
+		User user = builder.id(1).userName("GPonce").name("Gustavo").lastName("Ponce").email("test@test.com")
 				.build();
 		String userName = "GPonce";
 		when(mockUserRepository.findByUserName(userName)).thenReturn(user);
 		User result = userServiceUnderTest.findUserByUserName(userName);
 		assertNotNull(result);
 		assertEquals(userName, result.getUserName());
+		assertNotNull(user.toString());
+		assertNotNull(builder.toString());
+        EqualsVerifier.forClass(user.getClass()).withIgnoredAnnotations(Id.class).verify();
 	}
 
 	@Test

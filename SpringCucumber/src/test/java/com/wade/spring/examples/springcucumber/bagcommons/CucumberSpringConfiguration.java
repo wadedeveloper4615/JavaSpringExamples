6a7b1191fc07adae08960ca7202005a0baf5a03d
WADEOS <wade4615@gmail.com>
2021-03-18 08:52:12 -0500

    added a rest project with angular

--- SpringCucumber/src/test/java/com/wade/spring/examples/springcucumber/bagcommons/CucumberSpringConfiguration.java
+++ SpringCucumber/src/test/java/com/wade/spring/examples/springcucumber/bagcommons/CucumberSpringConfiguration.java
@@ -1,9 +1,17 @@
 package com.wade.spring.examples.springcucumber.bagcommons;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
+
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CucumberSpringConfiguration {
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "junit:build/cucumber-junit.xml", "html:build/cucumber.html"},
+        features = "classpath:/"
+)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)public class CucumberSpringConfiguration {
 }

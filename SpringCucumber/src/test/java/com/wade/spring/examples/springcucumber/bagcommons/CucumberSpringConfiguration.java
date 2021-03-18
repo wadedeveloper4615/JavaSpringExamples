c220739398e4cb2d3538fd59b76177b9e9e9a3ff
WADEOS <wade4615@gmail.com>
2021-03-18 10:18:56 -0500

    project update

--- SpringCucumber/src/test/java/com/wade/spring/examples/springcucumber/bagcommons/CucumberSpringConfiguration.java
+++ SpringCucumber/src/test/java/com/wade/spring/examples/springcucumber/bagcommons/CucumberSpringConfiguration.java
@@ -9,9 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {"pretty", "junit:build/cucumber-junit.xml", "html:build/cucumber.html"},
-        features = "classpath:/"
-)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)public class CucumberSpringConfiguration {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class CucumberSpringConfiguration {
 }
